5e6304f665d94280cd66c801b1a46adca8f1745f
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-10 15:39:06 +0100

    try to make numberEl a col

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,38 +4,41 @@
 // per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 
-const rowEl = document.createElement ("div");
-rowEl.classList.add("row");
+const rowEl = document.querySelector(".row");
 
 const containerEl = document.querySelector(".container");
 
-containerEl.append(rowEl)
+containerEl.append(rowEl);
+
+
 
 // Create a for loop to console.log the numbers 
-for (let i =1; i<101; i++) {
-    const numberEl = document.createElement ("div");
-    numberEl.classList.add = ("col");
+for (let i = 1; i < 101; i++) {
+    const numberEl = document.createElement("div");
     // create a if condition for 3 multiples with % method
     //console.log fizz
-    if (i % 3 === 0 && i % 5 != 0 ) {
+    if (i % 3 === 0 && i % 5 != 0) {
         // i = "fizz"
         numberEl.append("Fizz");
         console.log("Fizz");
-    // create a else if condition for the 5 multiples with % method
-    //console.log buzz
+        // create a else if condition for the 5 multiples with % method
+        //console.log buzz
     } else if (i % 5 === 0 && i % 3 != 0) {
         numberEl.append("Buzz");
         console.log("Buzz");
-    // create a else if condition with && logical operator for 3 and 5 multiples 
-    //console.log fizzbuzz
+        // create a else if condition with && logical operator for 3 and 5 multiples 
+        //console.log fizzbuzz
     } else if (i % 3 === 0 && i % 5 === 0) {
         numberEl.append("FizzBuzz");
-        console.log ("FizzBuzz");
+        console.log("FizzBuzz");
     } else {
         numberEl.append(i)
         console.log(i)
     }
-    
+
+   
+
+    numberEl.classList.add = ("col");
     rowEl.append(numberEl)
 
 
