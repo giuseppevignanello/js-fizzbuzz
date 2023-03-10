50a8ddb570f995bf7ce99ef014e68f817226773a
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-10 15:23:25 +0100

    show elements on screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,28 +3,40 @@
 // per i multipli di 5 stampi “Buzz”
 // per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
-// Create a for loop to console.log the numbers 
 
+const rowEl = document.createElement ("div");
+rowEl.classList.add("row");
+
+const containerEl = document.querySelector(".container");
+
+containerEl.append(rowEl)
+
+// Create a for loop to console.log the numbers 
 for (let i =1; i<101; i++) {
-// create a if condition for 3 multiples with % method
+    const numberEl = document.createElement ("div");
+    numberEl.classList.add = ("col");
+    // create a if condition for 3 multiples with % method
     //console.log fizz
     if (i % 3 === 0 && i % 5 != 0 ) {
         // i = "fizz"
+        numberEl.append("Fizz");
         console.log("Fizz");
+    // create a else if condition for the 5 multiples with % method
+    //console.log buzz
     } else if (i % 5 === 0 && i % 3 != 0) {
-        console.log("Buzz")
+        numberEl.append("Buzz");
+        console.log("Buzz");
+    // create a else if condition with && logical operator for 3 and 5 multiples 
+    //console.log fizzbuzz
     } else if (i % 3 === 0 && i % 5 === 0) {
-        console.log ("FizzBuzz")
+        numberEl.append("FizzBuzz");
+        console.log ("FizzBuzz");
     } else {
+        numberEl.append(i)
         console.log(i)
     }
     
-
-// create a else if condition for the 5 multiples with % method
-    //console.log buzz
-// create a else if condition with && logical operator for 3 and 5 multiples 
-    //console.log fizzbuzz
-// create a else condition to console.log all others numbers
+    rowEl.append(numberEl)
 
 
 }
