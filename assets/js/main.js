469e0e57904f49d6cc9738d2d9e31de6d388459f
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-10 14:52:11 +0100

    problem analysis

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,21 @@
 // per i multipli di 3 stampi “Fizz” al posto del numero
 // per i multipli di 5 stampi “Buzz”
 // per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+// Create a for loop to console.log the numbers 
+// create a if condition for 3 multiples with % method
+    //console.log fizz
+// create a else if condition for the 5 multiples with % method
+    //console.log buzz
+// create a else if condition with && logical operator for 3 and 5 multiples 
+    //console.log fizzbuzz
+
+
+
+
+
+// tools
+// - for
+// -console.log
+//%
+//if
